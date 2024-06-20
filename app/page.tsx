58aaff30a627884884cf03f22a041670f660014d
Anton Sjöstrand <asjoa99@hotmail.com>
2024-06-20 09:48:45 +0200

    fix the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+
+      <div className="container mx-auto flex sm:hidden justify-center items-center">
+       <h1> Kategorier på mobil</h1>
+
+      </div>
     
     </main>
   );
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from 'react';
 import Link from 'next/link';
@@ -10,72 +10,119 @@ import CloseIcon from '@mui/icons-material/Close';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const toggleSearch = () => {
+    setSearchOpen(!searchOpen);
+  };
+
   return (
-    <nav className=" bg-custom-blue shadow-lg ">
+    <nav className="bg-custom-blue shadow-lg">
       <div className="container mx-auto">
-      <div className=" flex justify-center items-center pt-6">
+        {/* Logo and hamburger menu for small screens */}
+        <div className="flex justify-between items-center pt-6 md:hidden">
+          <Link href={'/'}>
             <Image
               width={190}
               height={40}
               src="/Gymiceps.png"
               alt="logga"
-           
+              className='cursor-pointer'
             />
+          </Link>
+          <button onClick={toggleMenu}>
+            {menuOpen ? <CloseIcon className='text-red-500' /> : <MenuIcon className='text-white' />}
+          </button>
+        </div>
+
+        {/* Search bar for small screens, always visible */}
+        <div className="md:hidden flex justify-center items-center mt-4">
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded"
+            placeholder="Sök..."
+          />
+        </div>
+
+        {/* Main navbar content for larger screens */}
+        <div className="hidden md:flex flex-col items-center">
+          <div className="flex justify-center items-center pt-6">
+            <Link href={'/'}>
+              <Image
+                width={190}
+                height={40}
+                src="/Gymiceps.png"
+                alt="logga"
+                className='cursor-pointer'
+              />
+            </Link>
           </div>
 
-        <div className="flex justify-between items-center py-4">
-          <div className='hidden md:flex space-x-6'>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-           <InstagramIcon />
-            </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <FacebookIcon/>
-            </a>
-
-          </div>
-            <div className="hidden md:flex space-x-6">
-              <Link href="/kosttillskott" className="hover:text-yellow-500">Kosttillskott</Link>
-              <Link href="/drycker" className="hover:text-yellow-500">Drycker</Link>
-              <Link href="/klader" className="hover:text-yellow-500">Kläder</Link>
-              <Link href="/traningstillbehor" className="hover:text-yellow-500">Träningstillbehör</Link>
+          <div className="flex justify-between items-center py-4 w-full">
+            <div className="flex space-x-6">
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <InstagramIcon className='text-white' />
+              </a>
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <FacebookIcon className='text-white' />
+              </a>
+              <a href="https://github.com/AntonSjostrand99" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <GitHubIcon className='text-white' />
+              </a>
             </div>
-        
-          <div className="hidden md:flex items-center space-x-6">
-         
-            <ShoppingBasketIcon />
-            <SentimentSatisfiedAltIcon />
-          </div>
-
-          <div className="md:hidden">
-            <button onClick={toggleMenu}>
-              {menuOpen ? <CloseIcon /> : <MenuIcon />}
-            </button>
+            <div className="flex space-x-6">
+              <Link href="/kosttillskott" className="text-white hover:text-yellow-500">Kosttillskott</Link>
+              <Link href="/drycker" className="text-white hover:text-yellow-500">Drycker</Link>
+              <Link href="/klader" className="text-white hover:text-yellow-500">Kläder</Link>
+              <Link href="/traningstillbehor" className="text-white hover:text-yellow-500">Träningstillbehör</Link>
+            </div>
+            <div className="flex items-center space-x-6">
+              <SearchIcon onClick={toggleSearch} className="transform transition-transform duration-200 hover:scale-110 text-white cursor-pointer" />
+              <ShoppingBasketIcon className="transform transition-transform duration-200 hover:scale-110 text-white cursor-pointer" />
+              <SentimentSatisfiedAltIcon className="transform transition-transform duration-200 hover:scale-110 text-white cursor-pointer" />
+            </div>
           </div>
         </div>
+
+        {/* Dropdown menu for small screens */}
         {menuOpen && (
-          <div className="md:hidden flex flex-col items-center space-y-4 mt-4">
-            <Link href="/kosttillskott" className="hover:text-yellow-500">Kosttillskott</Link>
-            <Link href="/drycker" className="hover:text-yellow-500">Drycker</Link>
-            <Link href="/klader" className="hover:text-yellow-500">Kläder</Link>
-            <Link href="/traningstillbehor" className="hover:text-yellow-500">Träningstillbehör</Link>
+          <div className="md:hidden flex flex-col items-center justify-center space-y-4 mt-4">
+            <Link href="/kosttillskott" className="text-white hover:text-yellow-500">Kosttillskott</Link>
+            <Link href="/drycker" className="text-white hover:text-yellow-500">Drycker</Link>
+            <Link href="/klader" className="text-white hover:text-yellow-500">Kläder</Link>
+            <Link href="/traningstillbehor" className="text-white hover:text-yellow-500">Träningstillbehör</Link>
             <div className="flex space-x-4">
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                <img src="/facebook-icon.png" alt="Facebook" className="w-6 h-6" />
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <FacebookIcon className='text-white' />
               </a>
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                <img src="/instagram-icon.png" alt="Instagram" className="w-6 h-6" />
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <InstagramIcon className='text-white' />
               </a>
-              <ShoppingBasketIcon />
-              <SentimentSatisfiedAltIcon />
+              <a href="https://github.com/AntonSjostrand99" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-200 hover:scale-110">
+                <GitHubIcon className='text-white' />
+              </a>
+              <ShoppingBasketIcon className="transform transition-transform duration-200 hover:scale-110 text-white" />
+              <SentimentSatisfiedAltIcon className="transform transition-transform duration-200 hover:scale-110 text-white" />
             </div>
+          </div>
+        )}
+
+        {/* Search bar for larger screens, toggled by search icon */}
+        {searchOpen && (
+          <div className="hidden md:flex justify-center items-center mt-4">
+            <input
+              type="text"
+              className="w-full md:w-1/2 p-2 border border-gray-300 rounded"
+              placeholder="Sök..."
+            />
           </div>
         )}
       </div>
